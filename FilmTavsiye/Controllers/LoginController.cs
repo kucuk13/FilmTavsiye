eb0a8473553960f1ba702881b999b002edc04096
kucuk13 <52529079+kucuk13@users.noreply.github.com>
2021-08-20 22:15:18 +0300

    Login and Logout

--- FilmTavsiye/Controllers/LoginController.cs
+++ FilmTavsiye/Controllers/LoginController.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using FilmTavsiye.Models.Dtos;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace FilmTavsiye.Controllers
 {
@@ -27,18 +28,38 @@ namespace FilmTavsiye.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Index(LoginDto loginDto)
+        public async Task<IActionResult> Index(LoginDto model)
         {
-            var claims = new List<Claim>
+            if (ModelState.IsValid)
             {
-                new Claim(ClaimTypes.Name, loginDto.UserName)
-            };
+                if (model.UserName.Equals("kaan") && model.Password.Equals("123"))
+                {
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, model.UserName)
+                    };
 
-            var userIdentity = new ClaimsIdentity(claims, "Login");
-            ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
-            await HttpContext.SignInAsync(principal);
-            
-            return RedirectToAction("Index", "Home", new { area = "Admin" });
+                    var userIdentity = new ClaimsIdentity(claims, "Login");
+                    ClaimsPrincipal principal = new ClaimsPrincipal(userIdentity);
+                    await HttpContext.SignInAsync(principal);
+
+                    return RedirectToAction("Index", "Home", new { area = "Admin" });
+                } 
+                else
+                {
+                    ModelState.AddModelError("", "User name or password is wrong.");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Login", new { area = "" });
         }
     }
 }
--- FilmTavsiye/Models/Dtos/LoginDto.cs
+++ FilmTavsiye/Models/Dtos/LoginDto.cs
@@ -10,10 +10,14 @@ namespace FilmTavsiye.Models.Dtos
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "{0} can not be empty.")]
         [Display(Name="User Name")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "{0} can not be empty.")]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
         [Display(Name ="Remember Me")]
         public bool RememberMe { get; set; }
     }
